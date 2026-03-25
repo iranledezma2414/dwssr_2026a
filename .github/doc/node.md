f85101474140be48f7515b4c37fecdda4e6c2af5
iranledezma2414 <iranledezma99@icloud.com>
2026-03-25 04:53:22 +0000

    ✨ feat: restructure project to use ESM, add server setup, and implement routing

--- .github/doc/node.md
+++ .github/doc/node.md
@@ -34,3 +34,25 @@ Migrar a ESM ofrece las siguientes ventajas:
 - Importaciones asíncronas con `import()`
 - Es el futuro del ecosistema.
 
+# 😈 Nodemon
+
+[Nodemon] (https://nodemon.io/) es un paquete que actua como
+un _wrapper_ (envoltorio) para Node.Js
+Su función es observar archivos en el 
+directorio de tu proyecto y reiniciar
+automaticamente la aplicación cuando
+detecta cambios guardados.
+
+# ✂️ Aliases
+
+Los **imports alisases** también conocidos como _path aliases_ o _module aliases_ 
+son atajos o alias que podemos configurar para simplificar la forma en que importamos
+modulos en que importamos modulos en nuestra aplicación.
+
+# Tarea 
+mover los siguientes archivos y directorios al directorio
+_server_:
+* 📁 bin
+* 📁 routes
+* 📁 views
+* 📄 app.js
--- server/app.js
+++ server/app.js
@@ -1,12 +1,17 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import createError from 'http-errors';
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import { fileURLToPath } from 'url';
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var authorRouter = require('./routes/author');
+import indexRouter from './routes/index.js';
+import usersRouter from './routes/users.js';
+import authorRouter from './routes/author.js';
+
+// Recreando __dirname que no existe por defecto en ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 var app = express();
 
@@ -18,7 +23,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+// Ajustamos el ../ porque app.js ahora está dentro de server/
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -40,4 +47,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+export default app;
