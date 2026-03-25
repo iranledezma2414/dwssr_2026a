f0206c394068f318bce57d5fc9a57b1edd29c205
iranledezma2414 <iranledezma99@icloud.com>
2026-03-25 16:55:51 +0000

    🔧 chore: vit configuration en progreso...

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+// importo la función de configuración de Vite
+import { defineConfig } from 'vite';
+
+// impporto un resolvedor de rutas
+import { resolve } from 'node:path';
+
+// exporto una instancia de configuración de Vite
+export default defineConfig({
